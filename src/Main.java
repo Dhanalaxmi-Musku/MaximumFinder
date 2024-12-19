cd5aff44f6b9a2dcb34cdc9157203c7c8da4e1da
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-19 21:46:08 +0530

    Added:Maximum finder for strings

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		 IntegerMaximum.testIntegerMax();
 		 FloatMaximum.testFloatMax();
+		 StringMaximum.testStringMax();
 	}
 
 }
--- src/StringMaximum.java
+++ src/StringMaximum.java
@@ -1,4 +1,21 @@
 
 public class StringMaximum {
+	public static String findMax(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) max = y;
+        if (z.compareTo(max) > 0) max = z;
+        System.out.println("Maximum string is: " + max);
+        return max;
+    }
+
+    public static void testStringMax() {
+        System.out.println("\nTesting String Maximum:");
+        String result1 = findMax("Peach", "Banana", "Apple");
+        System.out.println("TC 3.1 Pass: " + result1.equals("Peach"));
+        String result2 = findMax("Banana", "Peach", "Apple");
+        System.out.println("TC 3.2 Pass: " + result2.equals("Peach"));
+        String result3 = findMax("Apple", "Banana", "Peach");
+        System.out.println("TC 3.3 Pass: " + result3.equals("Peach"));
+    }
 
 }
